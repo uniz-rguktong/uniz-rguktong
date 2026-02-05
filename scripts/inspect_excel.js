aa958ca332cab8ba7677a7feeb1401786010a88a
sreecharan-desu <sreecharan309@gmail.com>
2026-02-05 16:54:28 +0530

    chore: cleanup scripts and tests, ignore .turbo

--- scripts/inspect_excel.js
+++
@@ -1,6 +0,0 @@
-const XLSX = require('xlsx');
-const workbook = XLSX.readFile('/Users/sreecharandesu/Projects/uniz-rguktong/tests/data/full_batch_grades.xlsx');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const data = XLSX.utils.sheet_to_json(sheet);
-const studentData = data.filter(r => r["Student ID"] === "O210008" || r["studentid"] === "O210008");
-console.log(JSON.stringify(studentData, null, 2));
